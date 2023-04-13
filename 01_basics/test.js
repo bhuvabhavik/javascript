6571c2a501e6acb54ea2008d745946be9859ee8f
Bhavik Bhuva <bhavikbhuva80@gmail.com>
2023-04-13 11:51:48 +0530

    changes

---
+++ 01_basics/test.js
@@ -0,0 +1,17 @@
+console.log("bhavik");
+
+
+const accountId  = 144553
+let accountEmail = "bhavik@google.com"
+var accountPassword = "234432"
+accountCity = "Toronto"
+
+
+console.log(accountId);
+console.log("bhavik");
+
+
+
+
+console.log(accountId);
+console.table([accountEmail,accountEmail,accountCity]);
